88df281d18546dbfb40db20c5dae40c241e5d2cc
Lumpiluk <lumpiluk@gmail.com>
2016-09-24 20:08:42 +0200

    Set up npm and browserify with babelify and browserify-shader.

--- Readme.md
+++ Readme.md
@@ -2,11 +2,17 @@
 
 ## Installation
 
-This project uses bower for managing dependencies, which are listed in bower.json. After cloning this project's repository, simply run "bower install".
+This project uses bower for managing client-side dependencies, which are listed in bower.json. After cloning this project's repository, simply run `bower install`.
+For development dependencies, the project makes use of the Node.js Package Manager. After cloning, run `npm install` for both development tools and client-side tools which are required by some of the former. To install only the latter, run `npm install --production`.
 
-Additional requirements:
+Development dependencies (mostly run automatically in WebStorm's File Watchers):
 
-- For WebStorm file watchers:
-    - *babel* for compiling ECMAScript 6 into ES5 compliant Javascript. Apparently, even modern browsers don't support ES6 modules yet. (sudo npm install -g --save-dev babel-cli. The necessary preset has already been installed via npm install --save-dev babel-preset-es2015)
-    - *lessc* for compiling Less CSS into regular CSS. (sudo npm install -g less)
-    - yuicompressor for minifying Javascript. (sudo npm install -g yuicompressor)
+    - *babel* for compiling ECMAScript 6 into ES5 compliant Javascript. Apparently, even modern browsers don't support ES6 modules yet.
+    - *lessc* for compiling Less CSS into regular CSS.
+    - *browserify* for a "require" that works in the browser. Otherwise, individual .js files would quickly become bloated.
+    - *babelify*: A combination of babel and browserify.
+    - *browserify-shader* plugin for browserify for including external shader text files in javascript.
+    - yuicompressor for minifying Javascript.
+
+Command for browserify with babelify and browserify-shader as it is set in the corresponding File Watcher:
+`browserify -t [browserify-shader --parameterize=true] -t [babelify --presets es2015] $FileName$ -o $FileNameWithoutExtension$.compiled.js`
--- resources/js/visualizations.compiled.js
+++ resources/js/visualizations.compiled.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -18,11 +19,16 @@ var Visualization = function () {
 
         this.$container = $container;
         this.fov = 45;
+        this.min_focal_length = 10; // for zooming, assuming full frame (35mm) camera sensor
+        this.max_focal_length = 400; // for zooming
+        this.zoom_steps = 20; // (if available)
+        this.zoom_sensitivity = 0.25; // For mouse wheels. Lower => more sensitive.
         this.aspect = $container.width() / $container.height();
         this.near = 0.1;
         this.far = 10000;
         this.renderer = new THREE.WebGLRenderer();
         this.scene = new THREE.Scene();
+        // this.axis_helper = new THREE.AxisHelper(5);
         this.pivot = new THREE.Object3D(); // Pivot for rotation of camera and lights.
         this.camera = new THREE.PerspectiveCamera(this.fov, this.aspect, this.near, this.far);
         this.camera.position.set(0, 0, 0, 1);
@@ -30,6 +36,7 @@ var Visualization = function () {
         this.camera.lookAt(this.scene.position);
         this.pivot.add(this.camera);
         this.scene.add(this.pivot);
+        // this.scene.add(this.axis_helper);
         this.renderer.setClearColor(0x505050, 1);
         this.renderer.setSize(this.$container.width(), this.$container.height());
 
@@ -42,10 +49,16 @@ var Visualization = function () {
         /* Initialize navigation controls. */
         this.current_rotation = new THREE.Euler(0, 0, 0, "YXZ"); // YXZ for no "sideways" rotation.
         this.starting_rotation = new THREE.Euler(0, 0, 0, "YXZ");
-        this.rotating = false;
+        this.starting_focal_length = 0;
+        this.dragging = false;
+        this.two_fingers_touching = false;
         this.drag_start = new THREE.Vector2(0, 0);
+        this.scale_start_distance = 0;
         this.$container.mousedown(function (event) {
             that.on_mouse_down.call(that, event);
+        });
+        this.$container.on("wheel", function (event) {
+            that.on_wheel.call(that, event);
         });
         this.mouse_move_handler = function (event) {
             // Will be added to document on mouse down.
@@ -75,6 +88,18 @@ var Visualization = function () {
             this.renderer.render(this.scene, this.camera);
         }
     }, {
+        key: "update_rotation",
+        value: function update_rotation(delta_x, delta_y) {
+            this.current_rotation.y = (this.starting_rotation.y + delta_y) % (2 * Math.PI);
+            this.current_rotation.x = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, (this.starting_rotation.x + delta_x) % (2 * Math.PI)));
+            this.pivot.rotation.copy(this.current_rotation);
+        }
+    }, {
+        key: "update_scale",
+        value: function update_scale(focal_length_delta) {
+            this.camera.setFocalLength(Math.max(this.min_focal_length, Math.min(this.max_focal_length, this.starting_focal_length + focal_length_delta)));
+        }
+    }, {
         key: "on_resize",
         value: function on_resize() {
             this.renderer.setSize(this.$container.width(), this.$container.height());
@@ -87,23 +112,15 @@ var Visualization = function () {
         value: function on_mouse_down(event) {
             this.drag_start.set(event.pageX, event.pageY);
             this.starting_rotation.copy(this.pivot.rotation);
-            this.rotating = true;
+            this.dragging = true;
             var that = this;
             document.addEventListener("mousemove", this.mouse_move_handler, false);
             document.addEventListener("mouseup", this.mouse_up_handler, false);
         }
     }, {
-        key: "update_rotation",
-        value: function update_rotation(delta_x, delta_y) {
-            this.current_rotation.y = (this.starting_rotation.y + delta_y) % (2 * Math.PI);
-            this.current_rotation.x = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, (this.starting_rotation.x + delta_x) % (2 * Math.PI)));
-            this.pivot.rotation.copy(this.current_rotation);
-            this.render();
-        }
-    }, {
         key: "on_mouse_move",
         value: function on_mouse_move(event) {
-            if (!this.rotating) {
+            if (!this.dragging) {
                 return;
             }
             event.preventDefault();
@@ -111,45 +128,89 @@ var Visualization = function () {
             var delta_y = -(event.pageX - this.drag_start.x) / $(window).width() * 2 * Math.PI;
             var delta_x = -(event.pageY - this.drag_start.y) / $(window).height() * 2 * Math.PI;
             this.update_rotation(delta_x, delta_y);
+            this.render();
         }
     }, {
         key: "on_mouse_up",
         value: function on_mouse_up(event) {
-            this.rotating = false;
+            this.dragging = false;
             document.removeEventListener("mousemove", this.mouse_move_handler, false);
             document.removeEventListener("mouseup", this.mouse_up_handler, false);
         }
     }, {
-        key: "on_touch_start",
-        value: function on_touch_start(event) {
-            switch (event.touches.length) {
-                case 1:
-                    /* One finger -> rotate! */
-                    event.preventDefault();
-                    this.drag_start.set(event.touches[0].pageX, event.touches[0].pageY);
-                    this.starting_rotation.copy(this.pivot.rotation);
-                    this.rotating = true;
-                    var that = this;
-                    document.addEventListener("mousemove", this.mouse_move_handler, false);
-                    document.addEventListener("mouseup", this.mouse_up_handler, false);
+        key: "on_wheel",
+        value: function on_wheel(event) {
+            var delta = -event.originalEvent.deltaY;
+            event.preventDefault();
+            switch (event.originalEvent.deltaMode) {
+                /* see https://developer.mozilla.org/en-US/docs/Web/Events/wheel */
+                case 0x00:
+                    // => Delta values in pixels.
+                    delta *= (this.max_focal_length - this.min_focal_length) / $(window).height();
                     break;
-                case 2:
-                    /* Two fingers -> zoom! */
-
+                case 0x01: // => Delta values in lines.
+                case 0x02:
+                    // => Delta values in pages.
+                    delta *= (this.max_focal_length - this.min_focal_length) / this.zoom_steps;
                     break;
             }
+            this.starting_focal_length = this.camera.getFocalLength();
+            this.update_scale(delta * this.zoom_sensitivity);
+            this.render();
+        }
+    }, {
+        key: "on_touch_start",
+        value: function on_touch_start(event) {
+            this.drag_start.set(event.touches[0].pageX, event.touches[0].pageY);
+            this.starting_rotation.copy(this.pivot.rotation);
+            this.dragging = true;
+            switch (event.touches.length) {
+                case 1:
+                    /* One finger -> rotate around y! */
+                    // event.preventDefault(); // Enabling this prevents scrolling.
+                    break;
+                case 2:
+                    /* Two fingers -> pinch to zoom, rotation around x! */
+                    event.preventDefault();
+                    this.scale_start_distance = new THREE.Vector2(event.touches[0].pageX, event.touches[0].pageY).distanceTo(new THREE.Vector2(event.touches[1].pageX, event.touches[1].pageY));
+                    this.starting_focal_length = this.camera.getFocalLength();
+                    this.two_fingers_touching = true;
+                    break;
+            }
+            /* Temporarily add listeners to document so that dragging also works outside the canvas. */
+            document.addEventListener("mousemove", this.mouse_move_handler, false);
+            document.addEventListener("mouseup", this.mouse_up_handler, false);
         }
     }, {
         key: "on_touch_move",
         value: function on_touch_move(event) {
-            if (!this.rotating) {
+            if (!this.dragging) {
                 return;
             }
-            event.preventDefault();
-            /* (y in delta_y is y-axis in 3D. Same for x in delta_x.) */
+
+            /*
+             * Rotation.
+             * (y in delta_y is y-axis in 3D. Same for x in delta_x.)
+             */
+            // TODO: use center point if two fingers are touching!
             var delta_y = -(event.touches[0].pageX - this.drag_start.x) / $(window).width() * 2 * Math.PI;
             var delta_x = -(event.touches[0].pageY - this.drag_start.y) / $(window).height() * 2 * Math.PI;
+            if (!this.two_fingers_touching) {
+                delta_x = 0;
+            }
             this.update_rotation(delta_x, delta_y);
+
+            /*
+             * Scale.
+             */
+            if (this.two_fingers_touching) {
+                event.preventDefault();
+                var distance = new THREE.Vector2(event.touches[0].pageX, event.touches[0].pageY).distanceTo(new THREE.Vector2(event.touches[1].pageX, event.touches[1].pageY));
+                var s_delta = (distance - this.scale_start_distance) / $(window).width() * (this.max_focal_length - this.min_focal_length);
+                this.update_scale(s_delta);
+            }
+
+            this.render();
         }
     }, {
         key: "on_touch_cancel",
@@ -158,6 +219,7 @@ var Visualization = function () {
         key: "on_touch_end",
         value: function on_touch_end(event) {
             this.on_mouse_up(event);
+            this.two_fingers_touching = false;
         }
     }]);
 
@@ -175,6 +237,11 @@ var RGBCubeVisualization = function (_Visualization) {
         _this.wireframe_cube_geometry = new THREE.BoxGeometry(1, 1, 1);
         _this.wireframe_cube = new THREE.BoxHelper(new THREE.Mesh(_this.wireframe_cube_geometry), 0xffffff);
         _this.scene.add(_this.wireframe_cube);
+
+        _this.rgb_cube_geometry = new THREE.BoxGeometry(1, 1, 1);
+        _this.rgb_cube_shader = require("../shaders/rgb-fragment.glsl");
+        console.log(_this.rgb_cube_shader());
+        //this.rgb_cube_mesh = new THREE.Mesh(this.rgb_cube_geometry, this.rgb_cube_mat);
         return _this;
     }
 
@@ -219,4 +286,16 @@ $(document).ready(function () {
     });
 });
 
-//# sourceMappingURL=visualizations-compiled.js.map
+},{"../shaders/rgb-fragment.glsl":2}],2:[function(require,module,exports){
+module.exports = function parse(params){
+      var template = "testtest \n" +
+" \n" 
+      params = params || {}
+      for(var key in params) {
+        var matcher = new RegExp("{{"+key+"}}","g")
+        template = template.replace(matcher, params[key])
+      }
+      return template
+    };
+
+},{}]},{},[1]);
