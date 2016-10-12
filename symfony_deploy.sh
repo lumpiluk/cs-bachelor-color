9c022cfeb3f277d18ac5ecf8e53f4d8a4058f93b
Lumpiluk <lumpiluk@gmail.com>
2016-10-12 23:44:44 +0200

    deploy script

--- symfony_deploy.sh
+++ symfony_deploy.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+echo Instead of php, you may have to run php5.5-cli
+echo Remember to configure app/config/parameters.yml. Normally, the command below does that automatically.
 php composer.phar install --no-dev --optimize-autoloader
