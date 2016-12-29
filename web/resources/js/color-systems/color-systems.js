f6cb939f482155c511035f33622a56084fac406d
Lumpiluk <lumpiluk@gmail.com>
2016-12-29 22:53:19 +0100

    HSL/HSV hue in degrees now also in exercises.

--- web/resources/js/color-systems/color-systems.js
+++ web/resources/js/color-systems/color-systems.js
@@ -9,8 +9,17 @@ export function get_color_system_by_name(color_system_name) {
         case "rgb": return new RGBColorSystem();
         case "cmy": return new CMYColorSystem();
         case "cmyk": return new CMYKColorSystem();
-        case "hsl": return new HSLColorSystem();
-        case "hsv": return new HSVColorSystem();
+        case "hsl":
+            let hsl_color = new HSLColorSystem();
+            hsl_color.properties[0].set_unit_scale(360); // set hue to degrees by default
+            return hsl_color;
+        case "hsv":
+            let hsv_color = new HSVColorSystem();
+            hsv_color.properties[0].set_unit_scale(360); // set hue to degrees by default
+            return hsv_color;
     }
+    /*
+    TODO: In exercises, when initializing sliders, also scale according to unit_scale!
+     */
     return null;
 }
